316578b0d4fb97f74a87d1bd6523f02017858e1c
“Amulya0912” <amulya92004@gmail.com>
2025-02-14 15:34:42 +0530

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+
+
+
+
 //import dotenv from "dotenv";
 //dotenv.config();
 
@@ -14,8 +19,13 @@ mongoose.connect("mongodb+srv://amulya2004:Amulya2004@cluster0.qfupa.mongodb.net
    });
 
 const app = express();
+app.use(express.json());
+
+
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000 !!! ");
 });
 
 app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
